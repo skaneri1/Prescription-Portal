064c0c66edda0e1a21c0d6939e3323c9b826b9dd
Shlok Kaneria <32154972+skaneri1@users.noreply.github.com>
2021-04-03 10:21:55 -0400

    Update performadd.php

--- performadd.php
+++ performadd.php
@@ -1,33 +1,34 @@
-<?php 
+<?php
     require "../configs/config.php";
     require "./common.php";
-        
+
         try {
         $connection = new PDO($dsn, $username, $password, $options);
 
-        $new_task = array(
-            "summary"       => escape($_POST['summary']),
-            "details"       => escape($_POST['details']),
-            "priority"      => escape($_POST['priority']),
-            "isComplete"    => escape($_POST['isComplete'])
+        $new_presc = array(
+            "name"       => escape($_POST['name']),
+            "freq"       => escape($_POST['freq']),
+            "instruc"      => escape($_POST['instruc']),
+            "filled"    => escape($_POST['filled']),
+            "lastfill"    => escape($_POST['lastfill'])
         );
 
-        if ($_POST['dueDate']) {
-            $new_task+= ["dueDate" => escape($_POST['dueDate'])];
+        if ($_POST['lastfill']) {
+            $new_presc+= ["lastfill" => escape($_POST['lastfill'])];
         }
 
         $sql = sprintf(
             "INSERT INTO %s (%s) values (%s)",
-            "tasks",
-            implode(", ", array_keys($new_task)),
-            ":" . implode(", :", array_keys($new_task)));
-        
+            "presc",
+            implode(", ", array_keys($new_presc)),
+            ":" . implode(", :", array_keys($new_presc)));
+
         $statement = $connection->prepare($sql);
-        $statement->execute($new_task);
-        
+        $statement->execute($new_presc);
+
         header ("location: /index.php");
         }
-    
+
         catch(PDOException $error) {
             echo $sql . "<br>" . $error->getMessage();
         }
