ec5ddbad55a104247bda6368c3bcf9e138623099
Shlok Kaneria <32154972+skaneri1@users.noreply.github.com>
2021-04-03 10:21:54 -0400

    Update performdelete.php

--- performdelete.php
+++ performdelete.php
@@ -6,19 +6,19 @@ require "./common.php";
     if (isset($_POST['submit'])) {
         try {
             $connection = new PDO($dsn, $username, $password, $options);
-           
+
             $id = escape($_GET['id']);
 
-            $sql = "DELETE FROM tasks WHERE id = :id";
-        
+            $sql = "DELETE FROM presc WHERE id = :id";
+
             $statement = $connection->prepare($sql);
             $statement->bindValue(':id', $id);
             $statement->execute();
 
             header ("location: /index.php");
-      
+
         } catch(PDOException $error) {
           echo $sql . "<br>" . $error->getMessage();
         }
-      } 
+      }
 ?>
