8c6cb0f03d2bab6457fd529792e82f5ae37cbe23
Shlok Kaneria <32154972+skaneri1@users.noreply.github.com>
2021-04-03 13:32:29 -0400

    Update showtask.php

--- templates/showtask.php
+++ templates/showtask.php
@@ -3,11 +3,11 @@
               <div class="card-header">
               <div class="row">
                   <div class="col-sm">
-                    <?php echo $pres["name"]; ?> <!-- else -->
+                    <?php echo $presc["name"]; ?> <!-- else -->
                   </div>
                   <div class="col-sm">
                       <?php
-                       switch($pres["filled"]) {
+                       switch($presc["filled"]) {
                           case 'Yes': ?>
                             <span class="badge float-right badge-success">Filled: Yes</span> <?php
                             break;
@@ -19,15 +19,15 @@
                 </div> <!-- Row #1 -->
                 </div> <!-- Header -->
                 <div class="card-body">
-                  <p class ="card-title"><?php echo $pres["freq"]; ?></p>
-                  <p class="card-text"><?php echo $pres["instruc"]; ?></p>
+                  <p class ="card-title"><?php echo $presc["freq"]; ?></p>
+                  <p class="card-text"><?php echo $presc["instruc"]; ?></p>
                 </div> <!-- Body -->
                 <div class="card-footer">
                   <div class = "row">
                       <div class = "col-sm">
                           <ul class="nav justify-content-end nav-pills card-header-pills">
                               <li class="nav-item">
-                                  <a class="nav-link" href="/deletetask.php?id=<?php echo $pres["id"] ?>"> <i class="fas fa-trash-alt"></i></a>
+                                  <a class="nav-link" href="/deletetask.php?id=<?php echo $presc["id"] ?>"> <i class="fas fa-trash-alt"></i></a>
                               </li>
                           </ul>
                       </div>
