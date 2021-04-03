6ab3b839fb51588d797bfa26f511680c8d4c6c9c
Shlok Kaneria <32154972+skaneri1@users.noreply.github.com>
2021-04-03 10:21:49 -0400

    Update nav.php

--- templates/nav.php
+++ templates/nav.php
@@ -1,9 +1,9 @@
 <nav class="navbar fixed-top navbar-expand-sm navbar-dark bg-primary">
-    <a class="navbar-brand" href="#">ToDo List</a>
+    <a class="navbar-brand" href="#">Prescription Portal</a>
     <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
         <div class="navbar-nav">
-        <a class="nav-item nav-link active" href="/">List Tasks</a>
-        <a class="nav-item nav-link active" href="/addtask.php">Add Task</a>
+        <a class="nav-item nav-link active" href="/">Patients</a>
+        <a class="nav-item nav-link active" href="/addtask.php">Prescriptions</a>
         </div>
     </div>
 </nav>
