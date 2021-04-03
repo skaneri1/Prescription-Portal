5933427d3d9abed984dc03232dbff8c44f5dfe08
Shlok Kaneria <32154972+skaneri1@users.noreply.github.com>
2021-04-03 11:12:36 -0400

    Update config.php

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
   $host       = "<endpoint>";
   $username   = "<username>";
   $password   = "<password>";
-  $dsn        = "mysql:host=$host;dbname=tasks";
+  $dsn        = "mysql:host=$host;dbname=presc";
   $options    = array(
                   PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
                 );
