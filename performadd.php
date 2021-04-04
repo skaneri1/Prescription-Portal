311c690e7c71e8f1cf1f575d3c9266436843c0c1
Shlok Kaneria <32154972+skaneri1@users.noreply.github.com>
2021-04-04 00:55:33 -0400

    Update performadd.php

--- performadd.php
+++ performadd.php
@@ -27,7 +27,7 @@
         $statement->execute($new_pres);
 
         header('Refresh:5; url=index.php');
-        echo "<h2 style='padding-left: 20px; padding-top: 20px; font-family: serif;'>".'Congratulations! The Prescription has
+        echo "<h2 style='padding-left: 20px; padding-top: 20px'>".'Congratulations! The Prescription has
         been saved.../nThis page will redirect back to patients in 5 seconds.'."</h2>";
         }
 
