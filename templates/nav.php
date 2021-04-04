bb4855c68f05e3f80bbc598fe0e4d14af5a88af6
Shlok Kaneria <32154972+skaneri1@users.noreply.github.com>
2021-04-04 10:41:18 -0400

    Update nav.php

--- templates/nav.php
+++ templates/nav.php
@@ -3,7 +3,9 @@
     <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
         <div class="navbar-nav">
         <a class="nav-item nav-link active" href="/">Patients</a>
-        <a class="nav-item nav-link active" href="/listpresc.php">Prescriptions</a>
+        <a class="nav-item nav-link active" href="/listpresc.php">Shlok's Prescriptions</a>
+        <a class="nav-item nav-link active" href="/listpresc1.php">Dhruv's Prescriptions</a>
+        <a class="nav-item nav-link active" href="/listpresc2.php">Hari's Prescriptions</a>
         </div>
     </div>
 </nav>
