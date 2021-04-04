0767666acbcfdac1772ae5b430f418d03a1f2aed
Shlok Kaneria <32154972+skaneri1@users.noreply.github.com>
2021-04-04 13:31:10 -0400

    Update listpresc.php

--- listpresc.php
+++ listpresc.php
@@ -12,6 +12,20 @@
 
         $result = $statement->fetchAll();
 
+		$sqla = "SELECT * FROM dhruv";
+
+		$statementa = $connection->prepare($sqla);
+		$statementa->execute();
+
+        $resulta = $statementa->fetchAll();
+
+		$sqlb = "SELECT * FROM hari";
+
+		$statementb = $connection->prepare($sql);
+		$statementb->execute();
+
+        $resultb = $statementb->fetchAll();
+
 	} catch(PDOException $error) {
 		echo $sql . "<br>" . $error->getMessage();
 	}
@@ -33,6 +47,31 @@
               require("./templates/showtask.php");
              } // <!-- foreach -->
           } ?> <!-- else -->
+
+
+			<h2 style="padding-bottom: 30px; padding-top: 30px">Dhruv's Prescriptions</h2>
+      <?php
+         if (!$resulta && $statementa->rowCount() == 0) { ?>
+          <h4> No prescriptions to display. Add one from the patients.</h4>
+         <?php }
+         else
+         {
+            foreach ($resulta as $presc) {
+              require("./templates/showtask.php");
+             } // <!-- foreach -->
+          } ?>
+
+			<h2 style="padding-bottom: 30px; padding-top: 30px">Hari's Prescriptions</h2>
+      <?php
+         if (!$resultb && $statementb->rowCount() == 0) { ?>
+          <h4> No prescriptions to display. Add one from the patients.</h4>
+         <?php }
+         else
+         {
+            foreach ($resultb as $presc) {
+              require("./templates/showtask.php");
+             } // <!-- foreach -->
+          } ?>
 			</div>
 
     <?php require("./templates/footer.php"); ?>
