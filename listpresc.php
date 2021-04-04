712e21cd16462e2e3550e6963fcefb43756adfdd
Shlok Kaneria <32154972+skaneri1@users.noreply.github.com>
2021-04-04 09:13:05 -0400

    Update listpresc.php

--- listpresc.php
+++ listpresc.php
@@ -12,8 +12,16 @@
 
         $result = $statement->fetchAll();
 
+		$sqla = "SELECT * FROM presc.dhruv";
+
+		$statementa = $connection->prepare($sqla);
+		$statementa->execute();
+
+        $resulta = $statementa->fetchAll();
+
 	} catch(PDOException $error) {
 		echo $sql . "<br>" . $error->getMessage();
+		echo $sqla . "<br>" . $error->getMessage();
 	}
 ?>
 
@@ -34,37 +42,18 @@
              } // <!-- foreach -->
           } ?> <!-- else -->
 
-		<?php
-			try {
-				require "../configs/config.php";
-				require "./common.php";
+			<h2 style="padding-bottom: 30px; padding-top: 30px">Dhruv's Prescriptions</h2>
+      <?php
+         if (!$resulta && $statementa->rowCount() == 0) { ?>
+          <h4> No prescriptions to display. Add one from the patients.</h4>
+         <?php }
+         else
+         {
+            foreach ($resulta as $presc) {
+              require("./templates/showtask.php");
+             } // <!-- foreach -->
+          } ?> <!-- else -->
 
-				$connection = new PDO($dsn, $username, $password, $options);
-
-				$sql = "SELECT * FROM presc.dhruv";
-
-				$statement = $connection->prepare($sql);
-				$statement->execute();
-
-		        $resultd = $statement->fetchAll();
-
-			} catch(PDOException $error) {
-				echo $sql . "<br>" . $error->getMessage();
-			}
-		?>
-
-		<h2 style="padding-bottom: 30px; padding-top: 30px;">Dhruv's Prescriptions</h2>
-		<?php
-			 if (!$resultd && $statement->rowCount() == 0) { ?>
-				<h4> No prescriptions to display. Add one from the patients.</h4>
-			 <?php }
-			 else
-			 {
-					foreach ($resultd as $presc) {
-						require("./templates/showtask.php");
-					 } // <!-- foreach -->
-				} ?> <!-- else -->
-
-		</div>
+			</div>
 
     <?php require("./templates/footer.php"); ?>
