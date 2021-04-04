e580573631fbddcc5c3f888ef4a3d7277e42a2bf
Shlok Kaneria <32154972+skaneri1@users.noreply.github.com>
2021-04-04 10:41:11 -0400

    Update listpresc.php

--- listpresc.php
+++ listpresc.php
@@ -12,26 +12,8 @@
 
         $result = $statement->fetchAll();
 
-		$connectiona = new PDO($dsn, $username, $password, $options);
-		$sqla = "SELECT * FROM dhruv";
-
-		$statementa = $connectiona->prepare($sqla);
-		$statementa->execute();
-
-        $resulta = $statementa->fetchAll();
-
-		$connectionb = new PDO($dsn, $username, $password, $options);
-		$sqlb = "SELECT * FROM hari";
-
-		$statementb = $connectionb->prepare($sqlb);
-		$statementb->execute();
-
-        $resultb = $statementb->fetchAll();
-
 	} catch(PDOException $error) {
 		echo $sql . "<br>" . $error->getMessage();
-		echo $sqla . "<br>" . $error->getMessage();
-		echo $sqlb . "<br>" . $error->getMessage();
 	}
 ?>
 
@@ -51,31 +33,6 @@
               require("./templates/showtask.php");
              } // <!-- foreach -->
           } ?> <!-- else -->
-
-			<h2 style="padding-bottom: 30px; padding-top: 30px">Dhruv's Prescriptions</h2>
-      <?php
-         if (!$resulta && $statementa->rowCount() == 0) { ?>
-          <h4> No prescriptions to display. Add one from the patients.</h4>
-         <?php }
-         else
-         {
-            foreach ($resulta as $presc) {
-              require("./templates/showtask.php");
-             } // <!-- foreach -->
-          } ?> <!-- else -->
-
-			<h2 style="padding-bottom: 30px; padding-top: 30px">Hari's Prescriptions</h2>
-      <?php
-         if (!$resultb && $statementb->rowCount() == 0) { ?>
-          <h4> No prescriptions to display. Add one from the patients.</h4>
-         <?php }
-         else
-         {
-            foreach ($resultb as $presc) {
-              require("./templates/showtask.php");
-             } // <!-- foreach -->
-          } ?> <!-- else -->
-
 			</div>
 
     <?php require("./templates/footer.php"); ?>
