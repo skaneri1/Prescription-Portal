16b47a1ae3ad0f1b92bbf137d0cc83c5b5d9a0b5
Shlok Kaneria <32154972+skaneri1@users.noreply.github.com>
2021-04-03 21:52:38 -0400

    Update index.php

--- index.php
+++ index.php
@@ -34,10 +34,33 @@
 			<div class="modal-content">
 				<div class="modal-header">
 					<button type="button" class="close" data-dismiss="modal">&times;</button>
-					<h4 class="modal-title">Modal Header</h4>
+					<h2 class="modal-title">Modal Header</h4>
 				</div>
 				<div class="modal-body">
-					<p>Some text in the modal.</p>
+					<div class="card" style="width: 50%">
+	        <form method="post" style="padding: 20px" action="./performadd.php">
+	            <div class="form-group">
+	                <label for="name" class="text-primary">Drug Name</label>
+	                <input class="form-control" id="name" name="name">
+
+	                <label for="freq" class="text-primary">Frequency</label>
+	                <textarea class="form-control" id="freq" name="freq"></textarea>
+
+	                <label for="instruc" class="text-primary">Instructions</label>
+	                <textarea class="form-control" id="instruc" rows="3" name="instruc"></textarea>
+
+	                <label for="filled" class="text-primary">Prescription Filled</label>
+	                <select class="form-control" id="filled" name="filled">
+	                    <option selected>Yes</option>
+	                    <option>No</option>
+	                </select>
+
+	                <label for="lastfill" class="text-primary">Date of last fill:</label>
+	                <input type="date" class="form-control" id="lastfill" name="lastfill">
+	            </div>
+	                <input  type="submit" class="btn btn-primary" name="submit" value="Add Prescription">
+	        </form>
+	        </div>
 				</div>
 				<div class="modal-footer">
 					<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
