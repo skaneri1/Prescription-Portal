7fc26794644c8007f906bffeb7c2b5393d06e779
Shlok Kaneria <32154972+skaneri1@users.noreply.github.com>
2021-04-03 21:47:32 -0400

    Update index.php

--- index.php
+++ index.php
@@ -21,6 +21,7 @@
 <?php require("./templates/header.php"); ?>
 
 		<div class="container">
+		<?php require "./templates/nav.php" ?>
 		<h2>Modal Example</h2>
 		<!-- Trigger the modal with a button -->
 		<button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Open Modal</button>
