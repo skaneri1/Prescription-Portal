cdfbfe1597f72c7fff7d87e6e59ae7166a34a505
Shlok Kaneria <32154972+skaneri1@users.noreply.github.com>
2021-04-04 09:17:09 -0400

    Create performadddp.php

---
+++ performadddp.php
@@ -0,0 +1,48 @@
+<?php
+    require "../configs/config.php";
+    require "./common.php";
+
+        try {
+        $connection = new PDO($dsn, $username, $password, $options);
+
+        $new_pres = array(
+            "name"       => escape($_POST['name']),
+            "freq"       => escape($_POST['freq']),
+            "instruc"      => escape($_POST['instruc']),
+            "filled"    => escape($_POST['filled'])
+        );
+
+        if ($_POST['lastfill']) {
+            $new_pres+= ["lastfill" => escape($_POST['lastfill'])];
+        }
+        //If entry exists in shlok...header('Refresh:5; url=index.php');...echo 404 error and say record exists
+
+        //else...do all the rest
+        $sql = sprintf(
+            "INSERT INTO %s (%s) values (%s)",
+            "presc.dhruv",
+            implode(", ", array_keys($new_pres)),
+            ":" . implode(", :", array_keys($new_pres)));
+
+        $statement = $connection->prepare($sql);
+        $statement->execute($new_pres);
+
+        $sql = sprintf(
+            "INSERT INTO %s (%s) values (%s)",
+            "presc",
+            implode(", ", array_keys($new_pres)),
+            ":" . implode(", :", array_keys($new_pres)));
+
+        $statement = $connection->prepare($sql);
+        $statement->execute($new_pres);
+
+        header('Refresh:5; url=index.php');
+        echo "<h2 style='padding-left: 20px; padding-top: 20px'>".'Congratulations! The Prescription has
+        been saved!'."</h2>";
+        echo "<h2 style='padding-left: 20px; padding-top: 20px'>".'This page will redirect back to patients in 5 seconds.'."</h2>";
+        }
+
+        catch(PDOException $error) {
+            echo $sql . "<br>" . $error->getMessage();
+        }
+?>
