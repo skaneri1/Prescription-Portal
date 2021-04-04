9c8bbf357c00c9e02403dd94b6e5997667d4995d
Shlok Kaneria <32154972+skaneri1@users.noreply.github.com>
2021-04-04 09:40:34 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,23 +1,3 @@
-<?php
-	try {
-		require "../configs/config.php";
-		require "./common.php";
-
-		$connection = new PDO($dsn, $username, $password, $options);
-
-		$sql = "SELECT * FROM presc";
-
-		$statement = $connection->prepare($sql);
-		$statement->execute();
-
-        $result = $statement->fetchAll();
-
-	} catch(PDOException $error) {
-		echo $sql . "<br>" . $error->getMessage();
-	}
-?>
-
-
 <?php require("./templates/header.php"); ?>
 
 		<div class="container">
