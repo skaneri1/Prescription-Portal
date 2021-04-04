ecd60488ae97c5002c9ddfd0ec0b6f1ff842158d
Shlok Kaneria <32154972+skaneri1@users.noreply.github.com>
2021-04-03 22:07:09 -0400

    Update index.php

--- index.php
+++ index.php
@@ -22,9 +22,10 @@
 
 		<div class="container">
 		<?php require "./templates/nav.php" ?>
-		<h2>Modal Example</h2>
+		<h1>Patients Registered with Portal</h1>
+		<h4>Shlok Kaneria</h4>
 		<!-- Trigger the modal with a button -->
-		<button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Open Modal</button>
+		<button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Add Prescription</button>
 
 		<!-- Modal -->
 		<div class="modal fade" id="myModal" role="dialog">
@@ -33,7 +34,7 @@
 			<!-- Modal content-->
 			<div class="modal-content">
 				<div class="modal-header">
-					<h2 class="modal-title">Modal Header</h2>
+					<h2 class="modal-title">Shlok's Prescription</h2>
 					<button type="button" class="close" data-dismiss="modal">&times;</button>
 				</div>
 				<div class="modal-body">
@@ -59,13 +60,94 @@
 	            </div>
 	                <input  type="submit" class="btn btn-primary" name="submit" value="Add Prescription">
 	        </form>
-	        </div>
-				</div>
-				<div class="modal-footer">
-					<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-				</div>
+	      </div>
 			</div>
+		</div>
+		</div>
 
+
+		<h4>Dhruv Parthasarathy</h4>
+		<!-- Trigger the modal with a button -->
+		<button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Add Prescription</button>
+
+		<!-- Modal -->
+		<div class="modal fade" id="myModal" role="dialog">
+		<div class="modal-dialog">
+
+			<!-- Modal content-->
+			<div class="modal-content">
+				<div class="modal-header">
+					<h2 class="modal-title">Dhruv's Prescription</h2>
+					<button type="button" class="close" data-dismiss="modal">&times;</button>
+				</div>
+				<div class="modal-body">
+	        <form method="post" style="padding: 20px" action="./performadd.php">
+	            <div class="form-group">
+	                <label for="name" class="text-primary">Drug Name</label>
+	                <input class="form-control" id="name" name="name">
+
+	                <label for="freq" class="text-primary">Frequency</label>
+	                <textarea class="form-control" id="freq" name="freq"></textarea>
+
+	                <label for="instruc" class="text-primary">Instructions</label>
+	                <textarea class="form-control" id="instruc" rows="3" name="instruc"></textarea>
+
+	                <label for="filled" class="text-primary">Prescription Filled</label>
+	                <select class="form-control" id="filled" name="filled">
+	                    <option selected>Yes</option>
+	                    <option>No</option>
+	                </select>
+
+	                <label for="lastfill" class="text-primary">Date of last fill:</label>
+	                <input type="date" class="form-control" id="lastfill" name="lastfill">
+	            </div>
+	                <input  type="submit" class="btn btn-primary" name="submit" value="Add Prescription">
+	        </form>
+	      </div>
+			</div>
+		</div>
+		</div>
+
+
+		<h4>Hari Dhanushkodi</h4>
+		<!-- Trigger the modal with a button -->
+		<button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Add Prescription</button>
+
+		<!-- Modal -->
+		<div class="modal fade" id="myModal" role="dialog">
+		<div class="modal-dialog">
+
+			<!-- Modal content-->
+			<div class="modal-content">
+				<div class="modal-header">
+					<h2 class="modal-title">Hari's Prescription</h2>
+					<button type="button" class="close" data-dismiss="modal">&times;</button>
+				</div>
+				<div class="modal-body">
+	        <form method="post" style="padding: 20px" action="./performadd.php">
+	            <div class="form-group">
+	                <label for="name" class="text-primary">Drug Name</label>
+	                <input class="form-control" id="name" name="name">
+
+	                <label for="freq" class="text-primary">Frequency</label>
+	                <textarea class="form-control" id="freq" name="freq"></textarea>
+
+	                <label for="instruc" class="text-primary">Instructions</label>
+	                <textarea class="form-control" id="instruc" rows="3" name="instruc"></textarea>
+
+	                <label for="filled" class="text-primary">Prescription Filled</label>
+	                <select class="form-control" id="filled" name="filled">
+	                    <option selected>Yes</option>
+	                    <option>No</option>
+	                </select>
+
+	                <label for="lastfill" class="text-primary">Date of last fill:</label>
+	                <input type="date" class="form-control" id="lastfill" name="lastfill">
+	            </div>
+	                <input  type="submit" class="btn btn-primary" name="submit" value="Add Prescription">
+	        </form>
+	      </div>
+			</div>
 		</div>
 		</div>
 
