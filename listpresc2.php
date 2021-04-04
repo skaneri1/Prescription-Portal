265beae572c96ab0d1f11cd2d594b396a7b6bcf0
Shlok Kaneria <32154972+skaneri1@users.noreply.github.com>
2021-04-04 10:41:16 -0400

    Create listpresc2.php

---
+++ listpresc2.php
@@ -0,0 +1,38 @@
+<?php
+	try {
+		require "../configs/config.php";
+		require "./common.php";
+
+		$connection = new PDO($dsn, $username, $password, $options);
+
+		$sql = "SELECT * FROM hari";
+
+		$statement = $connection->prepare($sql);
+		$statement->execute();
+
+        $result = $statement->fetchAll();
+
+	} catch(PDOException $error) {
+		echo $sql . "<br>" . $error->getMessage();
+	}
+?>
+
+
+<?php require("./templates/header.php"); ?>
+
+    <div class="container">
+			<?php require "./templates/nav.php" ?>
+			<h2 style="padding-bottom: 30px">Hari's Prescriptions</h2>
+      <?php
+         if (!$result && $statement->rowCount() == 0) { ?>
+          <h4> No prescriptions to display. Add one from the patients.</h4>
+         <?php }
+         else
+         {
+            foreach ($result as $presc) {
+              require("./templates/showtask.php");
+             } // <!-- foreach -->
+          } ?> <!-- else -->
+			</div>
+
+    <?php require("./templates/footer.php"); ?>
