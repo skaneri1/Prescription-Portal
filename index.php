1a15b6e858731ebd901cdcfd6a4f92523f62f0e2
Shlok Kaneria <32154972+skaneri1@users.noreply.github.com>
2021-04-03 21:41:39 -0400

    Update index.php

--- index.php
+++ index.php
@@ -20,19 +20,32 @@
 
 <?php require("./templates/header.php"); ?>
 
-    <div class="container">
-      <?php
-         require("./templates/nav.php");
-         if (!$result && $statement->rowCount() == 0) { ?>
-          <h4> No prescriptions to display. Add one from the patients.</h4>
-         <?php }
-         else
-         {
-            foreach ($result as $presc) {
-              require("./templates/showtask.php");
-             } // <!-- foreach -->
-          } ?> <!-- else -->
+		<div class="container">
+		<h2>Modal Example</h2>
+		<!-- Trigger the modal with a button -->
+		<button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Open Modal</button>
 
-    </div> <!-- container -->
+		<!-- Modal -->
+		<div class="modal fade" id="myModal" role="dialog">
+		<div class="modal-dialog">
+
+			<!-- Modal content-->
+			<div class="modal-content">
+				<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal">&times;</button>
+					<h4 class="modal-title">Modal Header</h4>
+				</div>
+				<div class="modal-body">
+					<p>Some text in the modal.</p>
+				</div>
+				<div class="modal-footer">
+					<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+				</div>
+			</div>
+
+		</div>
+		</div>
+
+		</div> <!-- container -->
 
     <?php require("./templates/footer.php"); ?>
